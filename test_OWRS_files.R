c0b71f9deff23deeb45b003841a340bb14fa0904
Vincent Hebble <vincent.hebble@laverne.edu>
2017-03-02 18:39:04 -0800

    Made it do more :)

--- test_OWRS_files.R
+++ test_OWRS_files.R
@@ -1,6 +1,11 @@
 library(dplyr)
+library(raster)
+library(rgdal)
 library(RateParser)
 library(yaml)
+
+this.dir <- dirname(parent.frame(2)$ofile) 
+setwd(this.dir)
 
 #import sample data and give the data parameters used in the owrs files that are not in the original data
 df_smc <- santamonica
@@ -11,13 +16,18 @@ df_sample <- tbl_df(df_smc) %>%
          et_amount = 2, wrap_customer = "No", irr_area = 2000, carw_customer = "No",
          season = "Winter", tax_exemption = "granted", lot_size_group = 3,
          temperature_zone = "Medium", pressure_zone = 1, water_font = "city_delivered",
-         area = "inside_city") %>% 
+         area = "inside_city", water_type = "potable", rate_class = "C1",
+         dwelling_units = 10, elevation_zone = 2, greater_than = "False",
+         usage_indoor_budget_ccf = .3) %>% 
   group_by(cust_class)
 #End
 
 
 #Declare the customer classes to be tested for each utility 
-customer_classes <- c("RESIDENTIAL_SINGLE", "RESIDENTIAL_MULTI", "COMMERCIAL")
+customer_classes <- c("RESIDENTIAL_SINGLE" 
+                      #"RESIDENTIAL_MULTI",
+                      #"COMMERCIAL"
+                      )
 #End
 
 #Retrieve the directories and files in the directores from the Open-Water-Specification-File directory 
@@ -75,55 +85,28 @@ df_NA <- data.frame(
 #End
 
 #Run through all the OWRS files and test if they are Readable and add the information to the data frame
-for(i in 1:(numOfFiles*length(customer_classes)))
+for(i in 1:(numOfFiles))
 {
-  #Determine the customer class to test
-  df_class <- df_sample %>% filter(cust_class == customer_classes[ceiling(i/numOfFiles)])
-  #End
-  
   #Open OWRS file and retrieve important data
   tryCatch(
     {
       #Declare Error Flags
       file_format_error <- TRUE
-      some_error <- TRUE
       #End
       
       #Set the index number for the file and directory vectors
-      if(i %% numOfFiles == 0)
-      {
-        index <- numOfFiles
-      }
-      else
-      {
-        index <- i %% numOfFiles
-      }
+      index <- i
       #End
       
       #Open OWRS file
       owrs_file <- getFileData(index)
       file_format_error <- FALSE
       #End
-
-      #Create a Data Frame with Utility Information and Calculated Bill Information
-      utility <- data.frame(utility_id = utilityID[index], utility_name = owrs_file$metadata$utility_name, bill_frequency = owrs_file$metadata$bill_frequency)
-      df_temp <- data.frame(utility, calculate_bill(df_class[1,], owrs_file))
-      #End
-      
-      #Check if service charge and commodity charge are in the data frame
-      df_temp <- hasServiceCharge(df_temp)
-      df_temp <- hasCommodityCharge(df_temp)
-      #End
-      
-      #Add Full Utility Bill Information to the bill Data Frame
-      df_bill <- rbind(df_bill, data.frame(df_temp[,c("utility_id", "utility_name", "bill_frequency", "usage_ccf", "cust_class", "service_charge", "commodity_charge", "bill")]))
-      some_error <- FALSE
-      #End
     },
     error = function(cond)
     {
       #Display Error Message for specific files
-      message(paste("Error in file:", file_names[index], "\n", cond, "\n"))
+      message(paste("Format error in file:", file_names[index], "\n", cond, "\n"))
       #End
     },
     finally =
@@ -132,24 +115,63 @@ for(i in 1:(numOfFiles*length(customer_classes)))
       if(file_format_error)
       {
         utility <- data.frame(utility_id = utilityID[index], utility_name = file_names[index], bill_frequency = "NA")
-      }
-
-      if(some_error)
-      {
         df_temp <- data.frame(utility, df_NA)
         df_bill <- rbind(df_bill, df_temp)
+        
+        #Remove Temporary Data Frame
+        rm(df_temp)
+        #End
       }
-      #End
-
-      #Remove Temporary Data Frame
-      rm(df_temp)
-      #End
+      else
+      {
+        for(j in 1:length(customer_classes))
+        {
+         tryCatch(
+           {
+              df_class <- df_sample %>% filter(cust_class == customer_classes[j])
+              
+              some_error <- TRUE
+              #Create a Data Frame with Utility Information and Calculated Bill Information
+              utility <- data.frame(utility_id = utilityID[index], utility_name = owrs_file$metadata$utility_name, bill_frequency = owrs_file$metadata$bill_frequency)
+              df_temp <- data.frame(utility, calculate_bill(df_class[1,], owrs_file))
+              #End
+              
+              #Check if service charge and commodity charge are in the data frame
+              df_temp <- hasServiceCharge(df_temp)
+              df_temp <- hasCommodityCharge(df_temp)
+              #End
+              
+              #Add Full Utility Bill Information to the bill Data Frame
+              df_bill <- rbind(df_bill, data.frame(df_temp[,c("utility_id", "utility_name", "bill_frequency", "usage_ccf", "cust_class", "service_charge", "commodity_charge", "bill")]))
+              some_error <- FALSE
+            },
+            error = function(cond)
+            {
+              #Display Error Message for specific files
+              message(paste("Other error in file:", file_names[index], "\n", cond, "\n"))
+              #End
+            },
+            finally =
+            {
+              #Check if another error occured
+              if(some_error)
+              {
+                df_temp <- data.frame(utility, df_NA)
+                df_bill <- rbind(df_bill, df_temp)
+              }
+              #End
+            
+              #Remove Temporary Data Frame
+              rm(df_temp)
+              #End
+            } 
+          )
+        }
+      }
     }
   )
 }
 #End
-
-
 
 
 #Format the Bill Information so that only valid data entries are presented, the decimal points are rounded, and the data is arranged by utility
@@ -160,12 +182,21 @@ df_final_bill$commodity_charge <- round(df_final_bill$commodity_charge, 2)
 df_final_bill$utility_name <- as.character(df_final_bill$utility_name)
 df_final_bill$bill_frequency <- as.character(df_final_bill$bill_frequency)
 
-df_final_bill <- df_final_bill %>% arrange(utility_id)
+df_final_bill <- df_final_bill %>% arrange(utility_name)
 #End
 
-#Output The Final Bill information to a CSV file
-write.csv(df_final_bill, "../../Dropbox/CSV/sampleBill.csv")
+shape <- readOGR(dsn = "../Shapefiles", layer = "service_areas_cadc_with_utility_id_points") 
+shape <- merge(shape, df_final_bill, by = "utility_id")
+
+setwd("../TempShapeFiles/")
+#Output The Final Bill information to a  shapefile
+writeOGR(shape, ".", "service_areas_cadc_points_with_billing_info", driver="ESRI Shapefile", overwrite_layer = TRUE)
 #End
+zipDemFiles <- list.files(recursive = TRUE)
+setwd("../")
+zip(zipfile = "../Dropbox/CSV/shapefileZip", files = paste("TempShapeFiles", zipDemFiles, sep = "/"))
+
+setwd("WaterRateTester/")
 
 #A function to check if a service charge is present and if not adds a 0 value to the Data Frame
 hasServiceCharge <- function(x) {
