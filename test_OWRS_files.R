e43d7fe1411be90b834cac833187c7e69bba5cbb
Vincent Hebble <vincent.hebble@laverne.edu>
2017-03-21 14:46:08 -0700

    Updated the Tester - Need to comment some of the code still

--- test_OWRS_files.R
+++ test_OWRS_files.R
@@ -1,8 +1,19 @@
 library(dplyr)
+library(ggplot2)
+library(scales)
 library(raster)
 library(rgdal)
 library(RateParser)
 library(yaml)
+
+setwd("../../Documents/WaterRateTester/")
+
+#Declare the customer classes to be tested for each utility 
+customer_classes <- c("RESIDENTIAL_SINGLE" 
+                      #"RESIDENTIAL_MULTI",
+                      #"COMMERCIAL"
+)
+#End
 
 #import sample data and give the data parameters used in the owrs files that are not in the original data
 df_smc <- santamonica
@@ -10,21 +21,24 @@ names(df_smc) <- c( "cust_id", "usage_ccf", "usage_month", "usage_year",  "cust_
 
 df_sample <- tbl_df(df_smc) %>%
   mutate(hhsize = 4, meter_size = '3/4"', usage_zone = 1, landscape_area = 2000,
-         et_amount = 2, wrap_customer = "No", irr_area = 2000, carw_customer = "No",
+         et_amount = 2.0, wrap_customer = "No", irr_area = 2000, carw_customer = "No",
          season = "Winter", tax_exemption = "granted", lot_size_group = 3,
          temperature_zone = "Medium", pressure_zone = 1, water_font = "city_delivered",
          area = "inside_city", water_type = "potable", rate_class = "C1",
          dwelling_units = 10, elevation_zone = 2, greater_than = "False",
-         usage_indoor_budget_ccf = .3) %>% 
+         usage_indoor_budget_ccf = .3, meter_type = "compound", block = 1,
+         tariff_area = 1, turbine_meter = "No") %>% 
   group_by(cust_class)
-#End
 
-
-#Declare the customer classes to be tested for each utility 
-customer_classes <- c("RESIDENTIAL_SINGLE" 
-                      #"RESIDENTIAL_MULTI",
-                      #"COMMERCIAL"
-)
+df_adjustable_sample <- tbl_df(data.frame (usage_ccf = 0, usage_month = 3, hhsize = 4, meter_size = '3/4"', usage_zone = 1, landscape_area = 2000,
+                                           et_amount = 2.0, wrap_customer = "No", irr_area = 2000, carw_customer = "No",
+                                           season = "Winter", tax_exemption = "granted", lot_size_group = 3,
+                                           temperature_zone = "Medium", pressure_zone = 1, water_font = "city_delivered",
+                                           area = "inside_city", water_type = "potable", rate_class = "C1",
+                                           dwelling_units = 10, elevation_zone = 2, greater_than = "False",
+                                           usage_indoor_budget_ccf = .3, meter_type = "compound", block = 1,
+                                           tariff_area = 1, turbine_meter = "No", cust_class = customer_classes[1]))
+test_usage_ccfs <- c(1, 10, 25, 50, 100, 200, 300)
 #End
 
 #Retrieve the directories and files in the directores from the Open-Water-Specification-File directory 
@@ -67,18 +81,20 @@ for(i in 1:numOfFiles)
 
 setwd("../../../WaterRateTester/")
 #End
-
-for(i in 1: length(error))
+if(length(error) > 0)
 {
-  directory_names <- directory_names[-error]
+  for(i in 1: length(error))
+  {
+    directory_names <- directory_names[-error]
+  }
+  numOfFiles <- numOfFiles - length(error)
 }
-numOfFiles <- numOfFiles - length(error)
 
 
 #Remove Previous Data and reinitialize the df_bill data frame
 rm(df_bill)
-df_bill <- data.frame(integer(), character(), character(), double(), character(), double(), double(), double())
-names(df_bill) <- c( "utility_id" ,"utility_name", "bill_frequency", "usage_ccf", "cust_class", "service_charge", "commodity_charge", "bill")
+df_bill <- data.frame(integer(), character(), character(), character(), double(), character(), double(), double(), double(), double())
+names(df_bill) <- c( "utility_id" ,"utility_name", "bill_frequency", "bill_type", "usage_ccf", "cust_class", "service_charge", "commodity_charge", "charge_ratio", "bill")
 #End
 
 #Declare Null Data Set
@@ -90,6 +106,7 @@ df_NA <- data.frame(
   #usage_date = NA,
   service_charge = NA,
   commodity_charge = NA,
+  charge_ratio = NA,
   bill = NA
 )
 #End
@@ -131,19 +148,28 @@ for(i in 1:(numOfFiles))
         #Remove Temporary Data Frame
         rm(df_temp)
         #End
-      }
-      else
+      } else
       {
         for(j in 1:length(customer_classes))
         {
           tryCatch(
             {
-              df_class <- df_sample %>% filter(cust_class == customer_classes[j])
+              #df_class <- df_sample %>% filter(cust_class == customer_classes[j])
+              df_adjustable_sample$usage_ccf <- 1
               
               some_error <- TRUE
               #Create a Data Frame with Utility Information and Calculated Bill Information
-              utility <- data.frame(utility_id = utilityID[index], utility_name = owrs_file$metadata$utility_name, bill_frequency = owrs_file$metadata$bill_frequency)
-              df_temp <- data.frame(utility, calculate_bill(df_class[1,], owrs_file))
+              if(owrs_file$rate_structure$RESIDENTIAL_SINGLE$commodity_charge == "flat_rate*usage_ccf")
+              {
+                utility <- data.frame(utility_id = utilityID[index], utility_name = owrs_file$metadata$utility_name, bill_frequency = owrs_file$metadata$bill_frequency,
+                                      bill_type = "Uniform")
+                df_temp <- data.frame(utility, calculate_bill(df_adjustable_sample[1,], owrs_file))
+              } else
+              {
+                utility <- data.frame(utility_id = utilityID[index], utility_name = owrs_file$metadata$utility_name, bill_frequency = owrs_file$metadata$bill_frequency,
+                                      bill_type = owrs_file$rate_structure$RESIDENTIAL_SINGLE$commodity_charge)
+                df_temp <- data.frame(utility, calculate_bill(df_adjustable_sample[1,], owrs_file))
+              }
               #End
               
               #Check if service charge and commodity charge are in the data frame
@@ -151,8 +177,31 @@ for(i in 1:(numOfFiles))
               df_temp <- hasCommodityCharge(df_temp)
               #End
               
+              chargeRatio <- df_temp$service_charge/df_temp$bill 
+              df_temp$charge_ratio <- round(chargeRatio, digits = 3)
+              
+              df_bill <- rbind(df_bill, data.frame(df_temp[,c("utility_id", "utility_name", "bill_frequency", "bill_type", "usage_ccf", "cust_class", "service_charge", "commodity_charge", "charge_ratio", "bill")]))
+              
               #Add Full Utility Bill Information to the bill Data Frame
-              df_bill <- rbind(df_bill, data.frame(df_temp[,c("utility_id", "utility_name", "bill_frequency", "usage_ccf", "cust_class", "service_charge", "commodity_charge", "bill")]))
+              for(k in seq(0, 300, 10))
+              {
+                df_temp_bill <- df_temp
+                df_adjustable_sample$usage_ccf <- k
+                df_temp_bill$usage_ccf <- k
+                
+                temp <- calculate_bill(df_adjustable_sample[1,], owrs_file)
+                
+                df_temp_bill$commodity_charge <- as.numeric(temp["commodity_charge"])
+                df_temp_bill$bill <- temp["bill"]
+                
+                chargeRatio <- df_temp$service_charge/df_temp_bill$bill 
+                df_temp_bill$charge_ratio <- round(chargeRatio, digits = 3)
+                
+                df_bill <- rbind(df_bill, data.frame(df_temp_bill[,c("utility_id", "utility_name", "bill_frequency", "bill_type", 
+                                                                     "usage_ccf", "cust_class", "service_charge", "commodity_charge",
+                                                                     "charge_ratio", "bill")]))
+                remove(df_temp_bill, temp)
+              }
               some_error <- FALSE
             },
             error = function(cond)
@@ -179,6 +228,7 @@ for(i in 1:(numOfFiles))
         }
       }
     }
+    
   )
 }
 #End
@@ -188,25 +238,100 @@ for(i in 1:(numOfFiles))
 df_final_bill <- tbl_df(df_bill) %>% filter(!is.na(bill)) 
 
 df_final_bill$bill <- round(as.numeric(df_final_bill$bill), 2)
-df_final_bill$commodity_charge <- round(df_final_bill$commodity_charge, 2)
+df_final_bill$commodity_charge <- round(as.numeric(df_final_bill$commodity_charge), 2)
+df_final_bill$service_charge <- round(df_final_bill$service_charge, 2)
 df_final_bill$utility_name <- as.character(df_final_bill$utility_name)
 df_final_bill$bill_frequency <- as.character(df_final_bill$bill_frequency)
 
 df_final_bill <- df_final_bill %>% arrange(utility_name)
 #End
 
-shape <- readOGR(dsn = "../Shapefiles", layer = "service_areas_cadc_with_utility_id_points") 
-shape <- merge(shape, df_final_bill, by = "utility_id")
+#shape <- readOGR(dsn = "../Shapefiles", layer = "service_areas_cadc_with_utility_id") 
+#shape <- merge(shape, df_final_bill, by = "utility_id")
 
-setwd("../TempShapeFiles/")
+#setwd("../../Dropbox/CSV/")
 #Output The Final Bill information to a  shapefile
-writeOGR(shape, ".", "service_areas_cadc_points_with_billing_info", driver="ESRI Shapefile", overwrite_layer = TRUE)
+#writeOGR(shape, ".", "service_areas_cadc_with_billing_info", driver="ESRI Shapefile", overwrite_layer = TRUE)
 #End
-zipDemFiles <- list.files(recursive = TRUE)
-setwd("../")
-zip(zipfile = "../Dropbox/CSV/shapefileZip", files = paste("TempShapeFiles", zipDemFiles, sep = "/"))
+#zipDemFiles <- list.files(recursive = TRUE)
+#setwd("../")
+#zip(zipfile = "../Dropbox/CSV/shapefileZip", files = paste("TempShapeFiles", zipDemFiles, sep = "/"))
 
-setwd("WaterRateTester/")
+#setwd("../../Documents/WaterRateTester/")
+
+NoBiMonthly <- nrow(df_final_bill %>% filter(bill_frequency == "bimonthly"))
+NoMonthly <- nrow(df_final_bill %>% filter(bill_frequency == "monthly"))
+NoOtherSchedule <- nrow(df_final_bill) - (NoBiMonthly + NoMonthly)
+
+if(NoOtherSchedule > 0)
+{
+  Schedule <- c("Monthly", "Bi-Monthly", "Other")
+  Schedule_DF <- data.frame(Schedule, c(NoMonthly, NoBiMonthly, NoOtherSchedule))
+  names(Schedule_DF) <- c("Bill_Frequency", "Value")
+} else {
+  Schedule <- c("2. Monthly", "1. Bi-Monthly")
+  Schedule_DF <- data.frame(Schedule, c(NoMonthly, NoBiMonthly))
+  names(Schedule_DF) <- c("Bill_Frequency", "Value")
+}
+
+
+billFrequencyPie <-ggplot(Schedule_DF, aes(x="1", y=Value, fill= Bill_Frequency)) +
+  geom_bar(width = 2, stat = "identity")+coord_polar("y", start=0) + scale_fill_brewer(palette="Blues") + theme_void() +
+  geom_text(aes(y = Value/2 + c(0, cumsum(Value)[-length(Value)]), label = percent(Value/nrow(df_final_bill))), size=5) +
+  ggtitle("Analysis of Bill Frequency")  + theme(plot.title = element_text(lineheight= .5, face="bold")) + labs(fill = "Bill Frequencies")
+
+
+meanBill <- round(mean(df_final_bill$bill), 2)
+meanService <- round(mean(df_final_bill$service_charge), 2)
+meanCommodity <- round(mean(df_final_bill$commodity_charge), 2)
+meanOther <- round(meanBill - (meanService + meanCommodity), 2)
+
+Mean <- c( "3. Service Charge", "2. Commodity Charge", "1. Other Charge")
+Mean_DF <- data.frame(Mean, c(meanService, meanCommodity, meanOther))
+names(Mean_DF) <- c("Charge_Structure", "Value")
+
+meanBillPie <-ggplot(Mean_DF, aes(x= 1, y=Value, fill= Charge_Structure)) +
+  geom_bar(width = .5, stat = "identity") + scale_fill_brewer(palette="Greens") + theme_void() +
+  geom_text(aes(y = Value/2 + c(0, cumsum(Value)[-length(Value)]), label = printCurrency(Value))) +
+  ggtitle("Mean Bill by Parts", subtitle = paste("The total mean bill for 16 usage ccf is ", printCurrency(meanBill), "0", sep = ""))  +
+  labs(fill = "Charges") +
+  theme(plot.title = element_text(lineheight=.8, face="bold"), plot.subtitle = element_text(lineheight = .8)) 
+
+noTiered <- nrow(df_final_bill %>% filter(bill_type == "Tiered"))
+noBudget <- nrow(df_final_bill %>% filter(bill_type == "Budget"))
+noUniform <- nrow(df_final_bill %>% filter(bill_type == "Uniform"))
+noOtherBillType <- nrow(df_final_bill) - (noTiered + noBudget + noUniform)
+
+if(noOtherBillType > 0)
+{
+  Structure <- c( "Budget", "Tiered", "Uniform", "Other")
+  Structure_DF <- data.frame(Structure, c(noBudget, noTiered, noUniform, noOtherBillType))
+  names(Schedule_DF) <- c("Rate_Structure", "Value")
+} else {
+  Structure <- c( "3. Uniform", "2. Tiered", "1. Budget")
+  Structure_DF <- data.frame(Structure, c(noUniform, noTiered, noBudget))
+  names(Structure_DF) <- c("Rate_Structure", "Value")
+}
+
+
+rateStructurePie <-ggplot(Structure_DF, aes(x="", y=Value, fill= Rate_Structure)) +
+  geom_bar(width = 1, stat = "identity") + coord_polar("y", start=0) + scale_fill_brewer(palette="Purples") + theme_void() +
+  geom_text(aes(x = "", y = Value/2 + c(0, cumsum(Value)[-length(Value)]), label = percent(Value/nrow(df_final_bill))), size=5) +
+  ggtitle("Analysis of Rate Structure")  + theme(plot.title = element_text(lineheight=.8, face="bold")) + labs(fill = "Rate Structures")
+
+meanChargeRatio <- round(mean(as.numeric(df_final_bill$charge_ratio)), 3)
+
+commodity_scatter <- ggplot(df_final_bill, aes(x=usage_ccf, y=commodity_charge, color=utility_name)) +
+                            geom_point(shape=1) +  
+                            geom_line() +
+                            labs(x = "Usage CCF", y = "Commodity Charge", color = "Utility") +
+                            ggtitle("Commodity Charge Vs. Usage CCF", subtitle = "At every 10 CCF from 0 to 300")
+
+bill_scatter <- ggplot(df_final_bill, aes(x=usage_ccf, y=bill, color=utility_name)) +
+                            geom_point(shape=1) +    
+                            geom_smooth(method = loess, se = FALSE)  + 
+                            labs(x = "Usage CCF", y = "Bill", color = "Utility") +
+                            ggtitle("Total Bill Vs. Usage CCF", subtitle = "At every 10 CCF from 0 to 300")
 
 #A function to check if a service charge is present and if not adds a 0 value to the Data Frame
 hasServiceCharge <- function(x) {
@@ -236,3 +361,9 @@ getFileData <- function(fileNumber){
                                 directory_names[fileNumber], "/", file_names[fileNumber]), collapse = '')))
 }
 #End
+
+
+printCurrency <- function(Number)
+{
+  paste('$', Number, sep = "")
+}
